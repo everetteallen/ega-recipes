ec02b3113a0daddb665a6ccb25b046f8987bd240
everetteallen <ega@ncsu.edu>
2020-09-10 18:25:17 -0400

    more indent
    indent on comment

--- PostProcessors/HangoutsChatJPUNotifier.py
+++ PostProcessors/HangoutsChatJPUNotifier.py
@@ -83,8 +83,8 @@ class HangoutsChatJPUNotifier(Processor):
 
         
         # JPU Summary
-
-         #try:
+"""
+         try: """
             jamfpackageuploader_summary_result = self.env.get("jamfpackageuploader_summary_result")
             version = jamfpackageuploader_summary_result["data"]["version"]
             category = jamfpackageuploader_summary_result["data"]["category"]
