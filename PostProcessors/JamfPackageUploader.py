5557703f39e8f69e8176a094d96c1b67e7b3709f
everetteallen <ega@ncsu.edu>
2020-09-10 18:44:19 -0400

    more debug
    with print

--- PostProcessors/JamfPackageUploader.py
+++ PostProcessors/JamfPackageUploader.py
@@ -525,7 +525,7 @@ class JamfPackageUploader(Processor):
         
 
         # output the summary
-        print("Outputting Summary...")
+        self.output("Outputting Summary...")
         self.env["pkg_name"] = self.pkg_name
         self.env["pkg_uploaded"] = True
         self.env["jamfpackageuploader_summary_result"] = {
@@ -540,8 +540,8 @@ class JamfPackageUploader(Processor):
                 "pkg_date": self.pkg_date
             },
         }
-        print("Printing result of jamfpackageuploader_summary_result")
-        print(self.env["jamfpackageuploader_summary_result"])
+        self.output("Printing result of jamfpackageuploader_summary_result")
+        self.output(self.env["jamfpackageuploader_summary_result"])
 
 
 if __name__ == "__main__":
