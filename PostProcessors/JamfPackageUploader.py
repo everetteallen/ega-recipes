18e7fe24c1ca6aa65c945fda249c3887ea0199d9
everetteallen <ega@ncsu.edu>
2020-09-11 13:37:33 -0400

    removing debug
    removing prints for debug

--- PostProcessors/JamfPackageUploader.py
+++ PostProcessors/JamfPackageUploader.py
@@ -526,7 +526,6 @@ class JamfPackageUploader(Processor):
         
 
         # output the summary
-        self.output("*********************Outputting Summary...")
         self.env["pkg_name"] = self.pkg_name
         self.env["pkg_uploaded"] = True
         self.env["jamfpackageuploader_summary_result"] = {
@@ -541,8 +540,6 @@ class JamfPackageUploader(Processor):
                 "pkg_date": self.pkg_date
             },
         }
-        self.output("*********************Printing result of jamfpackageuploader_summary_result")
-        self.output(self.env["jamfpackageuploader_summary_result"])
 
 
 if __name__ == "__main__":
