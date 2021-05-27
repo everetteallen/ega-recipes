d35f5886a31cd1452f89af3e9d473a22b14b8ee1
everetteallen <ega@ncsu.edu>
2021-05-27 11:27:32 -0400

    Syntax fix
    fixed print statement

--- PostProcessors/HangoutsChatJPUNotifier.py
+++ PostProcessors/HangoutsChatJPUNotifier.py
@@ -126,7 +126,7 @@ class HangoutsChatJPUNotifier(Processor):
             print("Category: %s" % category)
             print("TimeStamp: %s" % pkg_date)
         
-        text = f'{JPUTitle} {JSS_URL}\n {pkg_name} {version} \n Category {category}\n {pkg_date}
+        text = f'{JPUTitle} {JSS_URL}\n {pkg_name} {version} \n Category {category}\n {pkg_date}'
         if bugged:
             print(text)
         hangoutschat_data= {'text':text}
